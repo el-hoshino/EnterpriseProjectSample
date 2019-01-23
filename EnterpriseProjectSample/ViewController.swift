a54941d52ec08275cc692164a58e2e6c745e8faf
Elvis Shi <shonetesmith@gmail.com>
2019-01-23 20:20:40 +0900

    Xcode init project

---
+++ EnterpriseProjectSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EnterpriseProjectSample
+//
+//  Created by 史 翔新 on 2019/01/23.
+//  Copyright © 2019 Crazism. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ EnterpriseProjectSampleTests/EnterpriseProjectSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  EnterpriseProjectSampleTests.swift
+//  EnterpriseProjectSampleTests
+//
+//  Created by 史 翔新 on 2019/01/23.
+//  Copyright © 2019 Crazism. All rights reserved.
+//
+
+import XCTest
+@testable import EnterpriseProjectSample
+
+class EnterpriseProjectSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
